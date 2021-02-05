ef64b0949d8fd9af44dc15b85d543c52ea6c9b17
andydlindsay@gmail.com <andydlindsay@gmail.com>
2021-02-05 13:12:19 -0800

    add express router breakout directory

---
+++ breakout-express-router/router/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { getProducts } = require('./db/product-queries');
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
